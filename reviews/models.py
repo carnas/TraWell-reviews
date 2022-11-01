4255f7e0d958c94cec31135fc250b3873c543e34
carnas <julia13500@o2.pl>
2022-11-01 09:14:21 +0100

    Add created on field in review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 from rides.models import Ride
@@ -8,8 +10,9 @@ class Review(models.Model):
     review_id = models.AutoField(primary_key=True)
     stars = models.IntegerField()
     description = models.CharField(max_length=500, null=True, blank=True)
+    was_rated_driver = models.BooleanField()
+    created_on = models.DateField(default=date.today)
     reviewer = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='reviewer')
     rated = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rated')
-    was_rated_driver = models.BooleanField()
     ride = models.ForeignKey(Ride, on_delete=models.DO_NOTHING)
 
