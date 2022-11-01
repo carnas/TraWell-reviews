832103f605e1706c3c3af8452babdb66df9bb1f0
carnas <julia13500@o2.pl>
2022-11-01 12:26:39 +0100

    Change endpoint into viewset

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 
 from reviews.models import Review
 from reviews.serializers import ReviewListSerializer
@@ -9,16 +8,20 @@ from users.models import User
 from utils.authorization import is_authorized
 
 
-@api_view(['GET'])
-def get_reviews(request, user_id):
-    if is_authorized(request):
-        try:
-            rated_user = User.objects.get(user_id=user_id)
-        except User.DoesNotExist:
-            return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewListSerializer
+    queryset = Review.objects.all()
 
-        reviews = Review.objects.filter(rated=rated_user)
-        serializer = ReviewListSerializer(reviews, many=True)
-        return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
-    else:
-        return JsonResponse(status=status.HTTP_401_UNAUTHORIZED, data='Not authorized', safe=False)
+    @action(detail=False, methods=['get'], url_path=r'user_reviews/(?P<user_id>[^/.]+)', )
+    def user_reviews(self, request, user_id, *args, **kwargs):
+        if is_authorized(request):
+            try:
+                rated_user = User.objects.get(user_id=user_id)
+            except User.DoesNotExist:
+                return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
+
+            reviews = Review.objects.filter(rated=rated_user)
+            serializer = ReviewListSerializer(reviews, many=True)
+            return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
+        else:
+            return JsonResponse(status=status.HTTP_401_UNAUTHORIZED, data='Not authorized', safe=False)
