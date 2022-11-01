9d33f821a59335d917ada39bce036e774b7c314a
carnas <julia13500@o2.pl>
2022-11-01 16:25:51 +0100

    Add filtering by user_type and rating scope

--- reviews/views.py
+++ reviews/views.py
@@ -12,6 +12,21 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewListSerializer
     queryset = Review.objects.all()
 
+    def _filter_by_rating(self, request, reviews):
+        try:
+            rating_from = request.GET['rating_from']
+            reviews = reviews.filter(stars__gte=rating_from)
+        except KeyError:
+            pass
+
+        try:
+            rating_to = request.GET['rating_to']
+            reviews = reviews.filter(stars__lte=rating_to)
+        except KeyError:
+            pass
+
+        return reviews
+
     @action(detail=False, methods=['get'], url_path=r'user_reviews/(?P<user_id>[^/.]+)', )
     def user_reviews(self, request, user_id, *args, **kwargs):
         if is_authorized(request):
@@ -21,6 +36,20 @@ class ReviewViewSet(viewsets.ModelViewSet):
                 return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User with id={user_id} not found', safe=False)
 
             reviews = Review.objects.filter(rated=rated_user)
+            try:
+                user_ride_type = request.GET['user_type']
+                if user_ride_type == 'all':
+                    reviews = reviews
+                elif user_ride_type == 'driver':
+                    reviews = reviews.filter(was_rated_driver=True)
+                elif user_ride_type == 'passenger':
+                    reviews = reviews.filter(was_rated_driver=False)
+                else:
+                    return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data="Invalid user_type parameter",
+                                        safe=False)
+            except KeyError:
+                return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data=f"Missing user_type parameter", safe=False)
+            reviews = self._filter_by_rating(request, reviews)
             serializer = ReviewListSerializer(reviews, many=True)
             return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
         else:
