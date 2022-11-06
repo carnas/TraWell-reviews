1b6fdd8e2fc689ccd860230fe8d25ade2d746f12
carnas <julia13500@o2.pl>
2022-11-06 13:03:42 +0100

    Adjust code to env file usage

--- utils/authorization.py
+++ utils/authorization.py
@@ -1,7 +1,24 @@
-import jwt
-from jwt.exceptions import DecodeError, ExpiredSignatureError, InvalidIssuerError, InvalidAudienceError, InvalidIssuedAtError, InvalidSignatureError
+import os
 
-from utils.variables import PUBLIC_KEY, ALGORITHMS, JWT_OPTIONS, ISSUER_CLAIM, AUDIENCE_CLAIM
+import jwt
+from jwt import DecodeError, ExpiredSignatureError, InvalidIssuerError, InvalidAudienceError, InvalidIssuedAtError, \
+    InvalidSignatureError
+
+
+def decode_token(token: str):
+    try:
+        public_key = f"""-----BEGIN RSA PUBLIC KEY-----\n{os.environ.get("TOKEN_KEY")}\n-----END RSA PUBLIC KEY-----"""
+        issuer_claim = os.environ.get("ISSUER_CLAIM")
+        jwt.decode(token, public_key, algorithms=['RS256'], issuer=issuer_claim,
+                   audience='account', options={'verify_signature': True,
+                                                'verify_exp': True,
+                                                'verify_iss': True,
+                                                'verify_iat': True,
+                                                'verify_aud': True})
+        return True
+    except (DecodeError, ExpiredSignatureError, InvalidIssuerError, InvalidAudienceError, InvalidIssuedAtError,
+            InvalidSignatureError):
+        return False
 
 
 def is_authorized(request):
@@ -10,10 +27,4 @@ def is_authorized(request):
     except KeyError:
         return False
 
-    try:
-        jwt.decode(token, PUBLIC_KEY, algorithms=ALGORITHMS, issuer=ISSUER_CLAIM, audience=AUDIENCE_CLAIM,
-                   options=JWT_OPTIONS)
-        return True
-    except (DecodeError, ExpiredSignatureError, InvalidIssuerError, InvalidAudienceError, InvalidIssuedAtError, InvalidSignatureError):
-        return False
-
+    return decode_token(token)
