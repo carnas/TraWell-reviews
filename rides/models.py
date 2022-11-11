17ebc7b2369cc430b4c08e5fce083b9bf26b1b6c
carnas <julia13500@o2.pl>
2022-11-11 14:41:14 +0100

    Add city_from, city_to and start_date fields in ride model

--- rides/models.py
+++ rides/models.py
@@ -5,6 +5,9 @@ from users.models import User
 
 class Ride(models.Model):
     ride_id = models.AutoField(primary_key=True)
+    city_from = models.CharField(max_length=110)
+    city_to = models.CharField(max_length=110)
+    start_date = models.DateTimeField()
     driver = models.ForeignKey(User, related_name='driver', on_delete=models.SET_NULL, blank=False, null=True)
     passengers = models.ManyToManyField(User, blank=True, through='Participation')
 
