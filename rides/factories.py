6a11efbd261f25d1bb22fd18cf0442705cdfc57b
carnas <julia13500@o2.pl>
2022-11-11 14:43:44 +0100

    Update factories due to model changes

--- rides/factories.py
+++ rides/factories.py
@@ -1,4 +1,5 @@
 import factory.django
+import pytz
 
 from users.factories import UserFactory
 from . import models
@@ -8,6 +9,9 @@ class RideFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Ride
 
+    city_from = factory.Faker('city')
+    city_to = factory.Faker('city')
+    start_date = factory.Faker('future_datetime', tzinfo=pytz.timezone('Europe/Warsaw'))
     driver = factory.SubFactory(UserFactory)
 
     @factory.post_generation
