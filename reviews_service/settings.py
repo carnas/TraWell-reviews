bfe883fb4f6a9f8997957118cae4fd7dc5849d9b
carnas <julia13500@o2.pl>
2022-11-12 19:25:08 +0100

    Finish endpoint

--- reviews_service/settings.py
+++ reviews_service/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = "reviews_service.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
     }
 }
 
--- rides/views.py
+++ rides/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 
@@ -16,17 +15,17 @@ def get_not_rated_rides(request, user_id):
         try:
             token = request.headers['Authorization'].split(' ')[1]
             email = user_utils.decode_token(token)['email']
-            # switch to email
-            reviewer = User.objects.get(user_id=1)
+            reviewer = User.objects.get(email=email)
             user = User.objects.get(user_id=user_id)
         except KeyError:
             return JsonResponse(status=status.HTTP_400_BAD_REQUEST, data='Invalid token', safe=False)
         except User.DoesNotExist:
             return JsonResponse(status=status.HTTP_404_NOT_FOUND, data=f'User not found', safe=False)
 
-        rides_with_user = Ride.objects.filter(driver=user, passengers__in=[reviewer])
-        rides_with_user_and_reviewer = rides_with_user.filter(driver=reviewer, passengers__in=[user])
-        serializer = NotRatedRideSerializer(rides_with_user_and_reviewer, user=user, many=True)
+        rides_user_as_driver = Ride.objects.filter(driver=user, passengers__in=[reviewer])
+        rides_reviewer_as_driver = Ride.objects.filter(driver=reviewer, passengers__in=[user])
+        rides_with_user_and_reviewer = list(rides_user_as_driver) + list(rides_reviewer_as_driver)
+        serializer = NotRatedRideSerializer(rides_with_user_and_reviewer, context=user, many=True)
         return JsonResponse(status=status.HTTP_200_OK, data=serializer.data, safe=False)
     else:
         return JsonResponse(status=status.HTTP_401_UNAUTHORIZED, data='Not authorized', safe=False)
