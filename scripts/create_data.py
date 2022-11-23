6c74acfea5a80938c8025b28bf65031cb1e5b0b2
carnas <julia13500@o2.pl>
2022-11-23 16:55:01 +0100

    Adjust data scripts to rabbit

--- scripts/create_data.py
+++ scripts/create_data.py
@@ -2,10 +2,6 @@ import scripts.create_users
 import scripts.create_rides
 import scripts.create_reviews
 
-USERS_AMOUNT = 20
-RIDES_AMOUNT = 100
 REVIEWS_AMOUNT = 100
 
-scripts.create_users.create(USERS_AMOUNT)
-scripts.create_rides.create(RIDES_AMOUNT)
 scripts.create_reviews.create(REVIEWS_AMOUNT)
--- scripts/create_users.py
+++
@@ -1,6 +0,0 @@
-from users.factories import UserFactory
-
-
-def create(amount):
-    for user in range(amount):
-        UserFactory()
